77fbc5b0f4aed954161a763f89c5546ea2edf76d
Sebastian <sebastianfox81@gmail.com>
2022-01-27 16:01:36 -0500

    Created context provider and custom global hook

--- client/src/context.js
+++ client/src/context.js
@@ -1,1 +1,15 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+  return <AppContext.Provider value="hello world">{children}</AppContext.Provider>
+}
+
+// CUSTOM HOOK
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+}
+
+export { AppProvider }
